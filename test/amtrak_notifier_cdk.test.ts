061240e40c496a3b9c42838f9189a6ac7ba44802
Teagan Le <vnl6rj@virginia.edu>
2023-10-16 18:09:12 -0400

    Initial work

---
+++ test/amtrak_notifier_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmtrakNotifierCdk from '../lib/amtrak_notifier_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amtrak_notifier_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmtrakNotifierCdk.AmtrakNotifierCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
